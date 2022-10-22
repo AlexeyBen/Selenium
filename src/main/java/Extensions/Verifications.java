41c3e43f0105c5a93ff5b0f59ecaa0c9149786df
AlexeyBen <alexey.ben@gmail.com>
2022-10-22 10:56:32 +0300

    fix(SRV-1090,SRV-1260): fix adding element feat: checking email address doc: refact:

--- src/main/java/Extensions/Verifications.java
+++ src/main/java/Extensions/Verifications.java
@@ -4,13 +4,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 
 public class Verifications {
-    public static boolean verifySelect(WebElement we , String expected){
-        boolean success = false;
-        Select select =  new Select(we);
+    public static boolean verifySelect(WebElement we, String expected) {
+        Select select = new Select(we);
         String actualText = select.getFirstSelectedOption().getText();
-        if(expected.equals(actualText)){
-            success = true;
-        }
-        return success;
+       return  expected.equals(actualText) ? true : false;
     }
 }
